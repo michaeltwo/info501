d47bd678ab86c233a7a63013283f1dbdf34d0b14
michael_weihua_zheng <weihua.zheng@hotmail.com>
2024-12-02 18:45:23 -0500

    fixed

--- gui/sssproj/sssapp/model.py
+++ gui/sssproj/sssapp/model.py
@@ -4,3 +4,25 @@ from django.conf import settings
 # Load the trained model
 file_path = os.path.join(settings.BASE_DIR, 'sssapp', 'static', 'xgboost_model.pkl')
 model = joblib.load(file_path)
+
+#rescaler
+import joblib
+import pandas as pd
+from preprocessing import clean, split_external_data
+
+def rescale_gui_input(df, file_path):
+    df = df.copy()
+    scaler = joblib.load(file_path)
+    X = scaler.fit_transform(X_df)
+    X_df = pd.DataFrame(X, columns=X_df.columns)
+
+    return X_df
+
+
+if __name__ == '__main__':
+    
+    df = pd.read_csv(r'G:\My Drive\School\Current Classes\SSIE 548 - Healtchare Data Science\Project\Data\sbcdata.csv')
+    datasets = split_external_data(df)
+    df = datasets[0]
+    df = clean(df)
+    test = rescale_gui_input(df,r'scaler.pkl')
