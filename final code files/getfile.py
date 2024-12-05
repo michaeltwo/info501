abbd88131733334b5f591e5a285632a5d8f8a3c8
michael_weihua_zheng <weihua.zheng@hotmail.com>
2024-12-05 14:06:28 -0500

    fix a function

--- final code files/getfile.py
+++ final code files/getfile.py
@@ -1,18 +1,20 @@
 import gdown
 
+def getfile():
+
 # Google Drive 文件的共享链接
-drive_link = "https://drive.google.com/file/d/1fjZZboWTeADwr1QBqGp1SVEZ54it2bHR/view?usp=drive_link"
+    drive_link = "https://drive.google.com/file/d/1fjZZboWTeADwr1QBqGp1SVEZ54it2bHR/view?usp=drive_link"
 
 # 提取文件ID
-file_id = drive_link.split('/d/')[1].split('/')[0]
+    file_id = drive_link.split('/d/')[1].split('/')[0]
 
 # 生成可直接下载的链接
-download_url = f"https://drive.google.com/uc?id={file_id}"
+    download_url = f"https://drive.google.com/uc?id={file_id}"
 
 # 保存的文件名
-output_filename = "sbcdata.csv"
+    output_filename = "sbcdata.csv"
 
 # 下载文件
-gdown.download(download_url, output_filename, quiet=False)
+    gdown.download(download_url, output_filename, quiet=False)
+    print(f"文件已下载到当前目录，文件名为: {output_filename}")
 
-print(f"文件已下载到当前目录，文件名为: {output_filename}")
